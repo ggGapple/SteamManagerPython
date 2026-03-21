56b72f5600305a0f9fffa63f4514f8dca991b847
Kiran Sinha <kiran.lee.sinha@gmail.com>
2026-03-21 00:42:38 -0700

    Implement group functionality, minor bug fixes

--- SteamManager.py
+++ SteamManager.py
@@ -4,13 +4,13 @@ from config import apiKey, mySteamName
 import os
 
 def get_player_summary(steamid):
-    url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/"
+    url = "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/"
     params = {"key": apiKey, "steamids": steamid}
     r = requests.get(url, params=params)
     return r.json()["response"]["players"]
 
 def get_id_from_name(username):
-    url = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/"
+    url = "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/"
     params = {"key": apiKey, "vanityurl": username}
     r = requests.get(url, params=params)
     data = r.json()["response"]
@@ -18,11 +18,11 @@ def get_id_from_name(username):
         return data["steamid"]
     return None
 
-def get_games(steamid):
-    url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/"
+def get_games(mysteamid):
+    url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/"
     params = {
         "key": apiKey,
-        "steamid": steamid,
+        "steamid": mysteamid,
         "include_appinfo": True,
         "include_played_free_games": True
     }
@@ -30,18 +30,32 @@ def get_games(steamid):
     return r.json()["response"]["games"]
 
 def get_data():
-    game_data = get_games(steamid=mySteamId)
+    game_data = get_games(mysteamid=mySteamId)
     game_data.sort(key=lambda game: game["playtime_forever"], reverse = True)
-    initialize_ratings(game_data)
+    initialize(game_data)
     print("Data obtained")
     return game_data
+
+def get_data_from_config(game_data):
+    if os.path.exists("games.json"):
+        with open("games.json", "r") as f:
+            game_data = json.load(f)
+            print("Games retrieved from games.json")
 
 def display(game_data):
     for game in game_data:
         if game["rating"] == -1:
-            print(f"{BOLD}{game["name"]}:{RESET} {game["playtime_forever"]} mins playtime, unrated")
+            rating = "unrated"
         else:
-            print(f"{BOLD}{game["name"]}:{RESET} {game["playtime_forever"]} mins playtime, rated {game["rating"]} out of 10")
+            rating = f"rated {game["rating"]} out of 10"
+        if len(game["groups"]) > 0:
+            in_groups = ", in group(s) "
+            for item in game["groups"]:
+                in_groups += item + ", "
+            in_groups = in_groups[:-2]
+        else:
+            in_groups = ""
+        print(f"{BOLD}{game["name"]}:{RESET} {game["playtime_forever"]} mins playtime, " + rating + in_groups)
 
 def write():
     if not games:
@@ -52,7 +66,7 @@ def write():
     print("Saved")
 
 def add(game_data, name, playtime):
-    game_data.append({"name": name, "playtime_forever": playtime, "appid": None})
+    game_data.append({"name": name, "playtime_forever": playtime, "rating": -1, "groups": [], "appid": None})
     print("Added " + name + " to data, run write to upload")
 
 def edit_playtime(game_data, name, playtime):
@@ -81,9 +95,10 @@ def find(game_data, name):
             return True
     return False
 
-def initialize_ratings(game_data):
+def initialize(game_data):
     for game in game_data:
         game["rating"] = -1
+        game["groups"] = []
 
 def rate(game_data, game_to_rate, rating):
     for game in game_data:
@@ -107,7 +122,49 @@ def rate_all(game_data, ignore_already_rated = True):
         except ValueError:
             print("Sorry, that's not a number. We'll move on but you can rate again later with the 'rate' command")
 
+def del_group(game_data, group_name):
+    for game in game_data:
+        if group_name in game["groups"]:
+            game["groups"].remove(group_name)
+    groups.pop(group_name)
+    print("Removed " + group_name + " from data, run write to upload")
+
+def del_game_from_group(game_data, game_name, group):
+    hit = False
+    for game in game_data:
+        if game['name'] == game_name:
+            hit = True
+            if group not in game['groups']:
+                print(game + " was never in " + group)
+            else:
+                game['groups'].remove(group)
+                groups[group].remove(game)
+                print("Successfully removed " + game + " from "+ group)
+    if not hit:
+        print("Sorry, that's not a game name")
+
+def add_game_to_group(game_data, game_name, group):
+    hit = False
+    for game in game_data:
+        if game['name'] == game_name:
+            hit = True
+            if group in game['groups']:
+                print(game + " is already in " + group)
+            else:
+                game['groups'].append(group)
+                groups[group].append(game['name'])
+                print("Successfully added " + game_name + " to "+ group)
+    if not hit:
+        print("Sorry, that's not a game name")
+
+def print_members_of_group(group_name):
+    print("Members of " + group_name + ":")
+    for item in groups[group_name]:
+        print(item)
+
+
 games = []
+groups = {}
 BOLD = "\033[1m"
 RESET = "\033[0m"
 mySteamId = get_id_from_name(mySteamName)
@@ -125,7 +182,8 @@ while True:
     command = input("-")
     if command.lower() == "help":
         print("Commands: \n>config: creates a config file based on given Steam key and ID\n"+
-              ">getData: read all of the Steam data to a memory\n"+
+              ">getDataFromSteam: read all of the Steam data to a memory\n"+
+              ">getDataFromConfig: if a config file exists, read all data from it to a memory\n" +
               ">write: writes the data to a file (resets the file)\n"+
               ">display: prints the data\n"+
               ">add: adds a game to the data\n"+
@@ -133,14 +191,17 @@ while True:
               ">edit: edits a game's playtime or name\n"+
               ">rate: allows you to add a rating for a game\n"+
               ">rateAll: goes through all games in local memory and asks for a rating\n"+
+              ">group: allows you to create/delete/edit groups\n" +
               ">help: prints a list of commands\n"+
               ">quit: quits the program")
-    elif command.lower() == "getdata" or command.lower() == "get data":
+    elif command.lower() == "getdatafromsteam":
         games = get_data()
     elif command.lower() == "write":
         write()
     elif command.lower() == "display":
         display(games)
+    elif command.lower() == "getdatafromconfig":
+        get_data_from_config(games)
     elif command.lower() == "remove":
         removeWhat = input("Type the name of the game to remove: ")
         if not find(games, removeWhat):
@@ -191,10 +252,14 @@ while True:
             whatPlaytime = 0
         add(games, addWhat, whatPlaytime)
     elif command.lower() == "config":
-        apiKey = input("Type your API Key (obtainable here: https://steamcommunity.com/dev/apikey): ")
+        apiKey = input("Type your API Key (obtainable here: https://steamcommunity.com/dev/apikey) or 'stop' to exit: ")
+        if apiKey.lower() == "stop":
+            continue
         keyOrName = input("Do you have a vanity Steam ID? That is, when you view your profile on a browser, "
                           +"in the url is it .com/id/<some number> or .com/id/<some String>? Type 'id' if it's "+
-                          "a number, or 'name' if it's a String: ")
+                          "a number, 'name' if it's a String, or 'stop' to quit: ")
+        if keyOrName.lower() == "stop":
+            continue
         if keyOrName.lower() == "id":
             steamid = input("Type your SteamID: ")
         elif keyOrName.lower() == "name":
@@ -214,8 +279,65 @@ while True:
         elif ignoreRating.lower() == "n":
             rate_all(games,False)
         else:
-            print("Sorry, that's not a unrecognized command")
+            print("Sorry, that's not a recognized command")
+    elif command.lower() == "group":
+        doWhat = input("Do you want to create a group, delete a group, print a group, or edit members? "
+                       +"Type 'create', 'delete', 'print', or 'edit': ")
+        if doWhat.lower() == "create":
+            groupName = input("What do you want to call this group? ")
+            groups[groupName] = []
+            print("Created group "+groupName+", enter 'group' command again to add to it")
+        elif doWhat.lower() == "delete":
+            print("The current groups are: " + str(list(groups.keys())))
+            groupName = input("Which group would you like to delete (or enter anything else to not delete)? ")
+            if groupName in groups:
+                del_group[groupName]
+            else:
+                print("Sorry, that's not one of the groups")
+        elif doWhat.lower() == "edit":
+            addOrRemove = input("Do you want to add games to a group or remove games from a group?"
+                                +" Type 'add' or 'remove': ")
+            skip = False
+            if addOrRemove.lower() == "add":
+                print("The current groups are: " + str(list(groups.keys())))
+                groupName = input("What group do you want to add to? ")
+                if groupName not in groups:
+                    print("Sorry, that's not one of the groups")
+                else:
+                    for i in range(len(games)):
+                        gameToAdd = input("Type the name of a game to add or 'stop' to quit: ")
+                        if gameToAdd.lower() == "stop":
+                            skip = True
+                            break
+                        add_game_to_group(games,gameToAdd,groupName)
+            elif addOrRemove.lower() == "remove":
+                print("The current groups are: " + str(list(groups.keys())))
+                groupName = input("What group do you want to remove from? ")
+                if groupName not in groups:
+                    print("Sorry, that's not one of the groups")
+                else:
+                    for i in range(len(games)):
+                        gameToRemove = input("Type the name of a game to remove or 'stop' to quit: ")
+                        if gameToRemove.lower() == "stop":
+                            skip = True
+                            break
+                        del_game_from_group(games,gameToRemove,groupName)
+            else:
+                print("Sorry, that's not a recognized command")
+            if skip:
+                continue
+        elif doWhat.lower() == "print":
+            print("The current groups are: " + str(list(groups.keys())))
+            whatGroup = input("What group do you want to print? ")
+            if whatGroup not in groups:
+                print("Sorry, that's not one of the groups")
+            else:
+                print_members_of_group(whatGroup)
+        else:
+            print("Sorry, that's not a recognized command")
+            continue
     elif command.lower() == "quit":
+        print("Exited SteamManager")
         break
     else:
         print("Sorry, that's not a recognized command")
